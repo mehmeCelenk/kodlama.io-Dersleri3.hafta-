6ea4c6022cc89c86f04562560af8063e43e0303a
mehmetCelenk <95227625+mehmeCelenk@users.noreply.github.com>
2022-10-14 21:54:07 +0300

    Add files via upload

---
+++ Odev2/interfaceDemo/src/interfaceDemo/Worker.java
@@ -0,0 +1,20 @@
+package interfaceDemo;
+//bizim çalışanımız
+public class Worker implements IWorkable, IEatable, IPayble{
+
+	@Override
+	public void work() {
+		
+	}
+
+	@Override
+	public void eat() {
+		
+	}
+
+	@Override
+	public void pay() {
+		
+	}
+
+}
